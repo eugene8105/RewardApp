a033ec7fbb4cf8c33c9795df5f8e69e7918ffd58
Eugene Bobalo <ebobalo@ArgonInd.biz>
2020-12-29 14:36:33 -0600

    inserted some data into database

--- data.sql
+++ data.sql
@@ -1,10 +1,15 @@
 DROP TABLE IF EXISTS USER_DB;
 
 CREATE TABLE USER_DB (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  points INT NOT NULL
+                         id INT AUTO_INCREMENT  PRIMARY KEY,
+                         name VARCHAR(250) NOT NULL,
+                         points INT NOT NULL
 );
 
 INSERT INTO USER_DB (name, points) VALUES
-  ('Eugene', 10);
+('Eugene', 10);
+
+INSERT INTO USER_DB (name, points) VALUES
+('Matthew', 10);
+
+select * from USER_DB;
